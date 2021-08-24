71aeb38732cb69cdf377ef2b967083575b2e46a8
kenishiky <89154259+Radoharivola@users.noreply.github.com>
2021-08-24 23:43:13 +0300

    Update sql.sql

--- scriptDB/sql.sql
+++ scriptDB/sql.sql
@@ -31,7 +31,11 @@ create table User(
     email varchar(100) not null,
     passwd varchar(255) not null
 );
-
+create table admin(
+    idAdmin int not null AUTO_INCREMENT PRIMARY KEY,
+    nom varchar(100) not null,
+    passwd varchar(255) not null
+);
 create table Achat(
     idAchat int not null AUTO_INCREMENT PRIMARY KEY,
     idProduit int not null,
@@ -43,18 +47,28 @@ create table Achat(
     foreign key(idCaisse) references Caisse(idCaisse)
 );
 
-insert into Produit values(null,'','.jpg',);
-insert into Produit values(null,'','.jpg',);
-insert into Produit values(null,'','.jpg',);
-insert into Produit values(null,'','.jpg',);
-insert into Produit values(null,'','.jpg',);
-insert into Produit values(null,'','.jpg',);
-insert into Produit values(null,'','.jpg',);
-insert into Produit values(null,'','.jpg',);
-insert into Produit values(null,'','.jpg',);
-insert into Produit values(null,'','.jpg',);
-insert into Produit values(null,'','.jpg',);
-insert into Produit values(null,'','.jpg',);
-insert into Produit values(null,'','.jpg',);
-insert into Produit values(null,'','.jpg',);
-insert into Produit values(null,'','.jpg',);
+insert into Produit values(null,'sel','sel.jpg',200);
+insert into Produit values(null,'bob','bob.jpg',300);
+insert into Produit values(null,'beurre','beurre.jpg',4000);
+insert into Produit values(null,'savon pure','savon pure.jpg',400);
+insert into Produit values(null,'savon liquide','savon liquide.jpg',2500);
+
+insert into Produit values(null,'sèche cheveux','tsaloMaster.jpg',300000);
+insert into Produit values(null,'Grille Pain','grillePain.jpg',500000);
+insert into Produit values(null,'batteuse','smeg.jpg',120000);
+insert into Produit values(null,'Machine a cafe','coffeemachine.jpg',2000000);
+insert into Produit values(null,'Appareil photo','pdpMaster.jpg',6000000);
+insert into Produit values(null,'M1 potatoBook','potatoBook.jpg',1);
+insert into Produit values(null,'calulatrice casio','casio.jpg',80000);
+insert into Produit values(null,'auto radio','autoradio.jpg',1000000);
+insert into Produit values(null,'TV Stick','Stick.jpg',2000000);
+insert into Produit values(null,'mixer grinder','mixer grinder.jpg',50000);
+
+
+insert into category values(null,'Electronique');
+insert into category values(null,'Electroménager');
+insert into category values(null,'PPN');
+
+insert into caisse values(null,'1');
+insert into caisse values(null,'2');
+insert into caisse values(null,'3');
